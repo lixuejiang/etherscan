edce9bf0119dcffcb56eb3bb4856ef1eba6da0a9
lixuejiang <642898959@qq.com>
2018-01-16 21:25:42 +0800

    :fire: 增加数据库操作

---
+++ common/db.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sqlite:./dbname.db')
+
+function getModel(dbname) {
+  const sequelize = new Sequelize(`sqlite:./${dbname}.db`)
+  const Record = sequelize.define('record', {
+    rank: Sequelize.STRING,
+    address: Sequelize.STRING,
+    quantity:Sequelize.DECIMAL,
+    recordDate: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+  });
+  return Record;
+}
+
+
+// Record =  getModel('122')
+// let temp = {
+//   rank: '1',
+//   address: 'trs[i].children[1].innerText',
+//   quantity: 10,
+// }
+// Record.sync().then(() => {
+//   Record.create(temp)
+// });
+
+module.exports = {
+  getModel: getModel
+}
--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const puppeteer = require('puppeteer');
+const getModel = require('./common/db').getModel;
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  let contractId = '0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0'
+  let contractMap = {
+    'EOS': '0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0',
+  }
+  const Record = getModel('EOS')
   await page.goto('https://etherscan.io/token/generic-tokenholders2?a=0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0&s=1E%2B27&p=1');
   await page.mainFrame().waitForSelector('#maintable')
   let data = await page.evaluate(() => {
     let trs = document.querySelectorAll('#maintable tr')
     let result = []
     for (var i = 1; i < trs.length; i++) {
-        result.push({
-          rank: trs[i].children[0].innerText,
-          address: trs[i].children[1].innerText,
-          percentage: trs[i].children[2].innerText,
-        })
+      let temp = {
+        rank: trs[i].children[0].innerText,
+        address: trs[i].children[1].innerText,
+        quantity: trs[i].children[2].innerText,
+      }
+      result.push(temp)
     }
     return result
   });
-  console.log('data is ' + JSON.stringify(data))
+  await Record.sync()
+  await Record.bulkCreate(data)
   await browser.close();
 })();
